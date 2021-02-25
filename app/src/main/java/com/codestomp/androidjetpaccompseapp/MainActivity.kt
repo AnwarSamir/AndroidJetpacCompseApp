29aa323acb3d94e7b2e270e371f7945a1e624c44
Anwar Samir <anwarmadkour95@gmail.com>
2021-02-25 13:56:17 +0200

    - move to beta

--- app/src/main/java/com/codestomp/androidjetpaccompseapp/MainActivity.kt
+++ app/src/main/java/com/codestomp/androidjetpaccompseapp/MainActivity.kt
@@ -2,47 +2,32 @@ package com.codestomp.androidjetpaccompseapp
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.ClickableText
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.font
-import androidx.compose.ui.text.font.fontFamily
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import com.codestomp.androidjetpaccompseapp.ui.LoginActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.text.ClickableText
 
-import com.codestomp.androidjetpaccompseapp.ui.theme.AndroidJetpacCompseAppTheme
-import com.codestomp.androidjetpaccompseapp.ui.theme.typography
-import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            AndroidJetpacCompseAppTheme {
+            com.codestomp.androidjetpaccompseapp.ui.ui.theme.AndroidJetpacCompseAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background,modifier = Modifier.padding(16.dp)) {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.padding(16.dp)
+                ) {
                     makeButton()
                 }
             }
@@ -57,13 +42,15 @@ class MainActivity : AppCompatActivity() {
         },border = BorderStroke(1.dp, Color.Black),
               shape = RoundedCornerShape(5.dp),
                 modifier = Modifier.fillMaxWidth(),
-                enabled = false
+                enabled = true
 
 
         ) {
             // Text
             makeTextForButton()
         }
+
+
 
     }
 
--- app/src/main/java/com/codestomp/androidjetpaccompseapp/ui/LoginActivity.kt
+++ app/src/main/java/com/codestomp/androidjetpaccompseapp/ui/LoginActivity.kt
@@ -1,13 +1,41 @@
 package com.codestomp.androidjetpaccompseapp.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.ScrollScope
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.codestomp.androidjetpaccompseapp.ui.ui.theme.AndroidJetpacCompseAppTheme
 
 class LoginActivity : AppCompatActivity() {
@@ -17,22 +45,117 @@ class LoginActivity : AppCompatActivity() {
             AndroidJetpacCompseAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Hi From login ")
+                    // userNameInput()
+                    //makeFrame(this)
+                    makeVerticalLayout()
                 }
             }
         }
     }
 }
 
+// make linear layout
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun makeVerticalLayout()
+{
+
+
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Login to your account",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+            userNameInput()
+            userPasswordInput()
+            makeButton()
+        }
+
+
 }
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun DefaultPreview() {
-    AndroidJetpacCompseAppTheme {
-        Greeting("Android")
+fun makeFrame(loginActivity: LoginActivity)
+{
+    Box(modifier = Modifier.size(Dp(200f)).padding(10.dp).clip(RoundedCornerShape(10f)).background(Color.Blue).clickable(onClick ={
+        Toast.makeText(loginActivity, "From Box Number one ", Toast.LENGTH_SHORT).show()
+    }
+
+    ),contentAlignment = Alignment.Center){
+
+        Box(modifier = Modifier.size(100.dp).background(Color.White).clip(RoundedCornerShape(20f)),contentAlignment = Alignment.Center){
+            Text(
+                text = "Helle from the top of box 2",
+                fontSize = 12.sp
+            )
+        }
     }
 }
+
+
+
+
+var rememberUserNameValue:MutableState<TextFieldValue>?=null
+
+@Composable
+fun userNameInput() {
+   rememberUserNameValue = remember { mutableStateOf(TextFieldValue()) }
+    TextField(value = rememberUserNameValue!!.value,
+        placeholder = { androidx.compose.material.Text(text = "enter user name") },
+        leadingIcon = { Icon(Icons.Default.Email,"email icon ") },
+        onValueChange = {
+            rememberUserNameValue!!.value = it
+        },modifier = Modifier.padding(0.dp,16.dp,0.dp,0.dp).fillMaxWidth(),
+
+        )
+
+
+}
+
+
+var rememberUserPasswordValue:MutableState<TextFieldValue>?=null
+@Composable
+fun userPasswordInput() {
+    rememberUserPasswordValue = remember { mutableStateOf(TextFieldValue()) }
+    TextField(value = rememberUserPasswordValue!!.value,
+        placeholder = { androidx.compose.material.Text(text = "enter password") },
+        leadingIcon = { Icon(Icons.Default.Lock,"") },
+        onValueChange = {
+            rememberUserPasswordValue!!.value = it
+        },modifier = Modifier.padding(0.dp,16.dp,0.dp,0.dp).fillMaxWidth(),
+        visualTransformation = PasswordVisualTransformation()
+
+        )
+
+
+}
+
+@Composable
+fun makeButton()
+{
+    Button(onClick = {
+        Log.e("User input is ->","usernaame is : ${rememberUserNameValue!!.value.text},password : ${rememberUserPasswordValue!!.value.text}")
+    },border = BorderStroke(1.dp, Color.Black),
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier.fillMaxWidth().padding(0.dp,16.dp,0.dp,0.dp),
+        enabled = true
+
+
+    ) {
+        // Text
+        makeTextForButton()
+    }
+
+
+
+
+}
+
+@Composable
+fun makeTextForButton(){
+    Text(text = "Login")
+}
+
